798165e9c07c28633c2b4fdf83dcb8a4a433f2c4
Adrien Abbey <abbey.12@wright.edu>
2024-04-18 15:03:34 -0400

    Fixed parenthesis, formatting

--- chapter8/ch8-slide9.vhdl
+++ chapter8/ch8-slide9.vhdl
@@ -1,17 +1,17 @@
 PROCEDURE add_unsigned (
-    in1, in2 : IN bit_vector(7 DOWNTO 0);
-    sum : OUT bit_vector(7 DOWNTO 0);
+    in1, in2 : IN BIT_VECTOR(7 DOWNTO 0);
+    sum : OUT BIT_VECTOR(7 DOWNTO 0);
     Cout : OUT BOOLEAN) IS
 
-    VARIABLE sum_tmp : bit_vector(7 DOWNTO 0);
+    VARIABLE sum_tmp : BIT_VECTOR(7 DOWNTO 0);
     VARIABLE carry : BIT := '0';
 
 BEGIN
     FOR i IN sum_tmp'reverse_range LOOP -- This reverses the range (0 to 7)
         sum_tmp(i) := in1(i) XOR in2(i) XOR carry;
         carry := (in1(i) AND in2(i))
-            OR (carry AND (in1(i))
-            OR (carry AND (in2(i))));
+            OR (carry AND (in1(i)))
+            OR (carry AND (in2(i)));
     END LOOP;
 
     sum := sum_tmp;
